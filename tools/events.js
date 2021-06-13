366c219843014c14fc6f25b2c266b56e13101764
alexanderkeidel <alexanderkeidel@icloud.com>
2021-06-13 11:09:23 +0200

    added event-listener

---
+++ tools/events.js
@@ -0,0 +1,8 @@
+var myEventHandlerActiveTrue = function () {
+    console.log('I hear a scream true!');
+    
+  }
+var myEventHandlerActiveFalse = function () {
+    console.log('I hear a scream false!');
+  }  
+module.exports = {myEventHandlerActiveTrue,myEventHandlerActiveFalse}
